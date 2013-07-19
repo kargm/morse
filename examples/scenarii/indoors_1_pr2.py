2626664b32fdcd01ab50498c520612316acf8d6c
Michael Karg <kargm@in.tum.de>
2013-07-19 11:03:41 +0200

    [scenarii] Add PR2 indoors-1 scenario

---
+++ examples/scenarii/indoors_1_pr2.py
@@ -0,0 +1,40 @@
+from morse.builder import *
+
+# A PR2 robot to the scene
+james = BasePR2()
+james.add_interface('ros')
+james.translate(x=-3, y=2.7, z=0.0)
+
+motion = MotionXYW()
+james.append(motion)
+motion.add_interface('ros', topic='/cmd_vel')
+
+# An odometry sensor to get odometry information
+odometry = Odometry()
+james.append(odometry)
+odometry.add_interface('ros', topic="/odom")
+
+robot_pose = Pose()
+robot_pose.name = 'robot_pose'
+james.append(robot_pose)
+robot_pose.add_interface('ros')
+
+scan = Hokuyo()
+scan.translate(x=0.275, z=0.252)
+james.append(scan)
+scan.properties(Visible_arc = False)
+scan.properties(laser_range = 30.0)
+scan.properties(resolution = 1.0)
+scan.properties(scan_window = 180.0)
+scan.create_laser_arc()
+
+scan.add_interface('ros', topic='/base_scan')
+
+# Keyboard control
+keyboard = Keyboard()
+keyboard.name = 'keyboard_control'
+james.append(keyboard)
+
+# Set scenario
+env = Environment('indoors-1/indoor-1')
+env.aim_camera([1.0470, 0, 0.7854])
