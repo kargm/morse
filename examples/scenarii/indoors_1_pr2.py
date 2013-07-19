276ef7880f86d5bc518c918b9d7e76c372703c12
Michael Karg <kargm@in.tum.de>
2013-07-19 12:21:17 +0200

    [scenarii] Post Odometry instead of Pose for fake localization.

--- examples/scenarii/indoors_1_pr2.py
+++ examples/scenarii/indoors_1_pr2.py
@@ -16,7 +16,7 @@ odometry.add_interface('ros', topic="/odom")
 robot_pose = Pose()
 robot_pose.name = 'robot_pose'
 james.append(robot_pose)
-robot_pose.add_interface('ros')
+robot_pose.add_stream('ros', 'morse.middleware.ros.pose.OdometryPublisher')
 
 scan = Hokuyo()
 scan.translate(x=0.275, z=0.252)
@@ -35,5 +35,5 @@ keyboard.name = 'keyboard_control'
 james.append(keyboard)
 
 # Set scenario
-env = Environment('indoors-1/indoor-1')
+env = Environment('indoors-1/indoor-1-adapto')
 env.aim_camera([1.0470, 0, 0.7854])
