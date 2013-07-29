cbce0a869264402726cff40b9841f868c962959a
Michael Karg <kargm@in.tum.de>
2013-07-29 17:51:06 +0200

    [tutorial] Fix scripts for MORSE 1.0

--- src/morse/blender/human_interaction/navi_tut/PlaceOnStart.py
+++ src/morse/blender/human_interaction/navi_tut/PlaceOnStart.py
@@ -1,8 +1,8 @@
 from bge import logic
 
-co = logic.getCurrentController()
+co = blenderapi.controller()
 collision = co.sensors['Collision']
-objects = logic.getCurrentScene().objects
+objects = blenderapi.scene().objects
 human = objects['POS_EMPTY']
 
 def place(): 
